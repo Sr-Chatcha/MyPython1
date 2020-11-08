62e3ec5eed99606c9925125e6d14c7663b5dbcfb
Sr-Chatcha <74114410+Sr-Chatcha@users.noreply.github.com>
2020-11-08 13:27:12 +0700

    first commit

---
+++ demoCode/cell_style.py
@@ -0,0 +1,20 @@
+# %%
+def rectangle(width, height):
+    return width * height
+
+
+# %%
+print('Test run')
+
+# %%
+rectangle(4, 5)
+
+# %% [markdown]
+# # tips and tricks
+# ## visual studio code
+# ### editor
+# * visual studio code
+# * sublime
+# * vim
+
+# %%
---
+++ demoCode/demo.py
@@ -0,0 +1,5 @@
+print('Hello Python')
+for i in range(10):
+    print(i)
+print('End')
+print('test')
